e9cc2b1258569e4d23d36bcaef771917791663b2
LazyBev <lazy25yari@proton.me>
2026-06-21 01:20:10 +0100

    nodev device for monero-miner to use UEFI

--- hosts/monero-miner.nix
+++ hosts/monero-miner.nix
@@ -7,7 +7,7 @@
   networking.hostName = "monero-miner";
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
