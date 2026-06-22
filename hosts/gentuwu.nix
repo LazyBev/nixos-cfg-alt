de5a8462312523517f96d9f657e9e670027f3e90
LazyBev <lazy25yari@proton.me>
2026-06-22 07:24:21 +0100

    Merge gentuwu-base.nix into gentuwu.nix

--- hosts/gentuwu.nix
+++ hosts/gentuwu.nix
@@ -1,7 +1,17 @@
-{ config, lib, ... }: {
-  imports = [ ./gentuwu-base.nix ];
-
+{ config, lib, pkgs, ... }: {
   networking.hostName = "gentuwu";
+
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+  hardware.enableRedistributableFirmware = true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "vmd" "usbhid" ];
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" "fuse" ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
@@ -17,6 +27,18 @@
   }];
 
   boot.kernelParams = [ "nvidia_drm.modeset=1" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  virtualisation.docker.enable = true;
+  users.users.yari.extraGroups = [ "kvm" "libvirtd" "docker" ];
 
   environment.etc."xmrig/config.json".source = ../configs/xmrig/config-unified.json;
 }
